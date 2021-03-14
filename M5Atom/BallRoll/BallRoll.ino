d583c4d4fa55d30a90f14e0af73d9fc636b4a2b2
Tomoto Washio <tomoto.washio@hitachivantara.com>
2021-03-14 04:20:38 -0700

    Clean up BallRoll.ino

--- M5Atom/BallRoll/BallRoll.ino
+++ M5Atom/BallRoll/BallRoll.ino
@@ -1,15 +1,12 @@
 #include "M5Atom.h"
 
-bool IMU6886Flag = false;
-
 void setup()
 {
   M5.begin(true, false, true);
-  
-  if (M5.IMU.Init() != 0)
-    IMU6886Flag = false;
-  else
-    IMU6886Flag = true;
+  if (M5.IMU.Init() != 0) {
+    Serial.println("Failed to initialize IMU. Halting.");
+    while (true) {}
+  }
 }
 
 const float SCALE = 0.07;
@@ -35,34 +32,32 @@ float alternate(float t) {
 
 void loop()
 {
+  M5.update();
   if (M5.Btn.wasPressed()) {
     x = y = dx = dy = 0;
   }
 
-  if (IMU6886Flag == true)
-  {
-    M5.IMU.getAccelData(&ax, &ay, &az);
-    if (fabs(ax) + fabs(ay) > ACC_MIN) dx += ax * SCALE, dy += ay * SCALE;
-    x += dx, y += dy;
-    dx *= DECAY, dy *= DECAY;
+  M5.IMU.getAccelData(&ax, &ay, &az);
+  if (fabs(ax) + fabs(ay) > ACC_MIN) dx += ax * SCALE, dy += ay * SCALE;
+  x += dx, y += dy;
+  dx *= DECAY, dy *= DECAY;
 
-    if (x >=  BOUNDARY) x =  BOUNDARY, dx *= -BOUNCE;
-    if (x <= -BOUNDARY) x = -BOUNDARY, dx *= -BOUNCE;
-    if (y >=  BOUNDARY) y =  BOUNDARY, dy *= -BOUNCE;
-    if (y <= -BOUNDARY) y = -BOUNDARY, dy *= -BOUNCE;
+  if (x >=  BOUNDARY) x =  BOUNDARY, dx *= -BOUNCE;
+  if (x <= -BOUNDARY) x = -BOUNDARY, dx *= -BOUNCE;
+  if (y >=  BOUNDARY) y =  BOUNDARY, dy *= -BOUNCE;
+  if (y <= -BOUNDARY) y = -BOUNDARY, dy *= -BOUNCE;
 
-    float p = min(fabs(dx) + fabs(dy), 1.0);
-    float q = alternate(t);
-    int r = int(0xff * p);
-    int b = int(0xff * (1.0f - p) * q);
-    int g = int(0x80 * (1.0f - p) * (1.0 - q));
-    t += TRANSIT;
-    
-    M5.dis.clear();
-    M5.dis.drawpix(x + OFFSET, y + OFFSET, (g << 16) | (r << 8) | b);
-    // Serial.printf("%.2f,%.2f,%.2f,%.2f,%.2f\r\n", p, dx, dy, ax, ay);
-    // Serial.printf("%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\r\n", x, y, dx, dy, ax, ay);
-  }
+  float p = min(fabs(dx) + fabs(dy), 1.0);
+  float q = alternate(t);
+  int r = int(0xff * p);
+  int b = int(0xff * (1.0f - p) * q);
+  int g = int(0x80 * (1.0f - p) * (1.0 - q));
+  t += TRANSIT;
+  
+  M5.dis.clear();
+  M5.dis.drawpix(x + OFFSET, y + OFFSET, (g << 16) | (r << 8) | b);
+  // Serial.printf("%.2f,%.2f,%.2f,%.2f,%.2f\r\n", p, dx, dy, ax, ay);
+  // Serial.printf("%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\r\n", x, y, dx, dy, ax, ay);
+
   delay(10);
-  M5.update();
 }
